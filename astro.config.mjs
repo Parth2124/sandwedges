99e4fdde1464cad01154a5686f43539e15f74350
GuessCodee <parthkh02@gmail.com>
2026-01-22 12:48:25 +0530

    Initial responsive Astro cafe website

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from "@tailwindcss/vite";
+
+
+
+
+export default defineConfig({
+  output: "static",
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
+
